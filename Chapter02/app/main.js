d4e5027716aaa65316c569414c3a0ad753bc2134
Pick1a1username <20301273+Pick1a1username@users.noreply.github.com>
2019-10-14 17:46:45 +0900

    The app can be now accessed outside of container.

--- Chapter02/app/main.js
+++ Chapter02/app/main.js
@@ -3,6 +3,10 @@ var port = 8180;
 
 var httpModule = require('./modules/http-module');
 
-http.createServer(httpModule.handleRequest).listen(8180, '127.0.0.1', () => {
-  console.log('Started Node.js http server at http://127.0.0.1:8180');
+/**
+ * The host parameter is changed to `0.0.0.0` so that the app
+ * can be accessed outside of container.
+ */
+http.createServer(httpModule.handleRequest).listen(8180, '0.0.0.0', () => {
+  console.log('Started Node.js http server at http://0.0.0.0:8180');
 });
